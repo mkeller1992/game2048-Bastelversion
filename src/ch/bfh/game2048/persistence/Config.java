895aaed409af52785a62522cece2b705d0bcf0c1
mkeller1992 <matthias_keller@bluewin.ch>
2017-05-26 22:12:06 +0200

    fixed bug concerning accessing properties

--- src/ch/bfh/game2048/persistence/Config.java
+++ src/ch/bfh/game2048/persistence/Config.java
@@ -1,5 +1,7 @@
 package ch.bfh.game2048.persistence;
 
+import java.net.URL;
+
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.FileBasedConfiguration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
@@ -13,39 +15,46 @@ import javafx.collections.ObservableList;
 
 public class Config {
 
-	public final static String PROPERTIES_FILE_NAME = "src/ch/bfh/game2048/persistence/2048.properties";
+	public final static String PROPERTIES_FILE_NAME = "ch/bfh/game2048/persistence/2048.properties";
 	FileBasedConfigurationBuilder<FileBasedConfiguration> builder;
 	Configuration conf;
-	
-	
+
 	private static Config instance = new Config();
-	
-	private Config(){	
-		
-		conf = load(PROPERTIES_FILE_NAME);		
+
+	private Config() {
+
+		conf = load(getClass().getClassLoader().getResource(PROPERTIES_FILE_NAME));
 	}
-	
+
 	public void setProperty(String key, String attribute) {
 		conf.setProperty(key, attribute);
 	}
-	
+
 	public String getPropertyAsString(String propertyKey) {
 		return conf.getProperty(propertyKey).toString();
 	}
-	
+
 	public int getPropertyAsInt(String propertyKey) {
 		return Integer.parseInt(conf.getProperty(propertyKey).toString());
 	}
-	
+
 	public ObservableList<String> getPropertyAsObservableList(String keyValue) {
 		return FXCollections.observableArrayList(conf.getStringArray(keyValue));
 	}
-	
-	public Configuration load(String fileName) {
+
+	/**
+	 * - Establishes the connection to the properties-file
+	 * - Use URL to load config, so that it can be loaded from within a jar
+	 * 
+	 * @param url
+	 *            path to the game properties file (use URL instead of String for accessing inside jar)
+	 * @return
+	 */
+
+	public Configuration load(URL url) {
+
 		Parameters params = new Parameters();
-		builder = new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
-				.configure(params.properties().setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
-						.setFileName(fileName));
+		builder = new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class).configure(params.properties().setListDelimiterHandler(new DefaultListDelimiterHandler(';')).setURL(url));
 		try {
 			return builder.getConfiguration();
 
@@ -64,7 +73,7 @@ public class Config {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static Config getInstance() {
 		if (instance == null) {
 			synchronized (Config.class) {
@@ -74,5 +83,5 @@ public class Config {
 		}
 		return instance;
 	}
-	
+
 }
